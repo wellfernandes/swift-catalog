ca43a270f122264c5f4c9793e31a8d6780536b15
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-24 19:24:10 -0300

    feat: added config mongo db class and annotation to enable

--- src/main/java/br/com/wellitonleal/swiftcatalog/SwiftCatalogApplication.java
+++ src/main/java/br/com/wellitonleal/swiftcatalog/SwiftCatalogApplication.java
@@ -2,8 +2,10 @@ package br.com.wellitonleal.swiftcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SwiftCatalogApplication {
 
 	public static void main(String[] args) {
