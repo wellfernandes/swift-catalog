29a38a19c57439f98cb0394a21ef218fd9c76595
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-01 16:50:48 -0300

    feat: added get all, update and delete controller methods

--- src/main/java/br/com/wellitonleal/swiftcatalog/controllers/CategoryController.java
+++ src/main/java/br/com/wellitonleal/swiftcatalog/controllers/CategoryController.java
@@ -3,11 +3,11 @@ package br.com.wellitonleal.swiftcatalog.controllers;
 import br.com.wellitonleal.swiftcatalog.domain.category.Category;
 import br.com.wellitonleal.swiftcatalog.domain.category.CategoryDTO;
 import br.com.wellitonleal.swiftcatalog.services.CategoryService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
@@ -22,5 +22,23 @@ public class CategoryController {
     public ResponseEntity<Category> create(@RequestBody CategoryDTO categoryDTO) {
         Category newCategory = this.categoryService.create(categoryDTO);
         return ResponseEntity.ok(newCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll() {
+        List<Category>categories = this.categoryService.getAll();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> update(@PathParam("id") String id, @RequestBody CategoryDTO categoryDTO) {
+        Category updatedCategory = this.categoryService.update(id, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> update(@PathParam("id") String id) {
+     this.categoryService.delete(id);
+     return ResponseEntity.noContent().build();
     }
 }
