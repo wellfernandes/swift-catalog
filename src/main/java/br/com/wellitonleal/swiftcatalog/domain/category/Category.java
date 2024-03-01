ca7030404900f44f423e07a4a44cdf9017c56999
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-01 16:01:16 -0300

    feat: added category default constructor

--- src/main/java/br/com/wellitonleal/swiftcatalog/domain/category/Category.java
+++ src/main/java/br/com/wellitonleal/swiftcatalog/domain/category/Category.java
@@ -16,4 +16,10 @@ public class Category {
     private String title;
     private String description;
     private String ownerId;
+
+    public Category(CategoryDTO categoryDTO){
+        this.title = categoryDTO.title();
+        this.description = categoryDTO.description();
+        this.ownerId = categoryDTO.ownerId();
+    }
 }
