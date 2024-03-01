536dca92583f294a4345bb99234a91f3b2093967
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-01 15:32:14 -0300

    feat: added category class

---
+++ src/main/java/br/com/wellitonleal/swiftcatalog/domain/category/Category.java
@@ -0,0 +1,19 @@
+package br.com.wellitonleal.swiftcatalog.domain.category;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String ownerId;
+}
