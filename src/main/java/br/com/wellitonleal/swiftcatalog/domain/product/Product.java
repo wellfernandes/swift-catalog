813a9b67dbd1cbefbc62faddde1e6383c620c4c1
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-01 15:32:32 -0300

    feat: added product class

---
+++ src/main/java/br/com/wellitonleal/swiftcatalog/domain/product/Product.java
@@ -0,0 +1,22 @@
+package br.com.wellitonleal.swiftcatalog.domain.product;
+
+import br.com.wellitonleal.swiftcatalog.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String ownerId;
+    private Integer price;
+    private Category category;
+}
