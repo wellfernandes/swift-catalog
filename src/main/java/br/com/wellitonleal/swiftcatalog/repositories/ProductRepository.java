54e2927e77cb5213360ea32a460eb50b215956ca
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-01 15:36:54 -0300

    feat: added product repository

---
+++ src/main/java/br/com/wellitonleal/swiftcatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.com.wellitonleal.swiftcatalog.repositories;
+
+import br.com.wellitonleal.swiftcatalog.domain.product.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String>{
+}
