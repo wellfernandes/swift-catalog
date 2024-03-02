e728a9ce0ccbf421bd99623c514bbd15563db4bb
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-02 14:31:53 -0300

    feat: added category service

--- src/main/java/br/com/wellitonleal/swiftcatalog/services/CategoryService.java
+++ src/main/java/br/com/wellitonleal/swiftcatalog/services/CategoryService.java
@@ -7,6 +7,7 @@ import br.com.wellitonleal.swiftcatalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -24,6 +25,10 @@ public class CategoryService {
 
     public List<Category> getAll(){
         return this.categoryRepository.findAll();
+    }
+
+    public Optional<Category> getById(String id){
+        return this.categoryRepository.findById(id);
     }
 
     public Category update(String id, CategoryDTO categoryDTO){
