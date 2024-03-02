91a9dc27b29625b1304c74e483a0df3877e4196a
Welliton Fernandes Leal <wellfernandes@me.com>
2024-03-02 14:28:49 -0300

    feat: added product controller

---
+++ src/main/java/br/com/wellitonleal/swiftcatalog/controllers/ProductController.java
@@ -0,0 +1,44 @@
+package br.com.wellitonleal.swiftcatalog.controllers;
+
+import br.com.wellitonleal.swiftcatalog.domain.product.Product;
+import br.com.wellitonleal.swiftcatalog.domain.product.ProductDTO;
+import br.com.wellitonleal.swiftcatalog.services.ProductService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/products")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody ProductDTO productDTO) {
+        Product newProduct = this.productService.create(productDTO);
+        return ResponseEntity.ok(newProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        List<Product>products = this.productService.getAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathParam("id") String id, @RequestBody ProductDTO productDTO) {
+        Product updatedProduct = this.productService.update(id, productDTO);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> update(@PathParam("id") String id) {
+        this.productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
